e50744a81424100a07389747caae34219c0d4ff8
Student <student@bluc02kl120drv6.local>
2016-01-07 12:17:26 -0600

    Initial Commit

---
+++ Xcode Git hackwichTests/Xcode_Git_hackwichTests.swift
@@ -0,0 +1,36 @@
+//
+//  Xcode_Git_hackwichTests.swift
+//  Xcode Git hackwichTests
+//
+//  Created by Student on 1/7/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import XCTest
+@testable import Xcode_Git_hackwich
+
+class Xcode_Git_hackwichTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
